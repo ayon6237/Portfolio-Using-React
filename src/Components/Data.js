788c12b9f90b91c1e5a2f097c86f9307e43797ca
ayon6237 <140192947+ayon6237@users.noreply.github.com>
2025-06-10 19:16:16 +0600

    Update Data.js

--- src/Components/Data.js
+++ src/Components/Data.js
@@ -1,30 +1,30 @@
 const data=[
     {
-        image:"/Assets/card-images/image3.png",
+        image:"Assets/card-images/image3.png",
         name:"HTML5"
     },
     {
-        image:"/Assets/card-images/image4.png",
+        image:"Assets/card-images/image4.png",
         name:"CSS"
     },
     {
-        image:"/Assets/card-images/image2.png",
+        image:"Assets/card-images/image2.png",
         name:"Java Script"
     },
     {
-        image:"/Assets/card-images/image1.png",
+        image:"Assets/card-images/image1.png",
         name:"React JS"
     },
     {
-        image:"/Assets/card-images/image5.png",
+        image:"Assets/card-images/image5.png",
         name:"Node JS"
     },
     {
-        image:"/Assets/card-images/image6.png",
+        image:"Assets/card-images/image6.png",
         name:"Express JS"
     },
     {
-        image:"/Assets/card-images/image7.png",
+        image:"Assets/card-images/image7.png",
         name:"Mongo DB"
     }
 ]
